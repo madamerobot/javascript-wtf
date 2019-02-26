0e80d6e42610325a056c9fec22e5d86eee97e091
Valerie Fuchs <valeriefuchs@Valeries-MacBook-Pro-2.local>
2019-02-26 20:43:27 +0100

    Started to rebuild form submission

--- script.js
+++ script.js
@@ -15,5 +15,10 @@ window.addEventListener('load', function() {
 })
 
 
+window.addEventListener('load', function() {
+    const sendFormButton = document.getElementById('mc-embedded-subscribe')
+    console.log(sendFormButton)
+})
+
 
 //<form action="https://cafe-robot.us18.list-manage.com/subscribe/post?u=343737dedbadf272d034f2cc4&amp;id=52ec62aeee" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="" novalidate>
