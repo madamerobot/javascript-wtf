e5e77f76b8105a7bb92a1e1fcf21213a66dc2e57
Valerie Fuchs <valeriefuchs@Valeries-MacBook-Pro-2.local>
2019-02-27 20:21:13 +0100

    Implemented AJAX calls to Mailchimp and inpage validation

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
+/*
+ * Mailchimp AJAX form submit VanillaJS
+ * Vanilla JS
+ * Author: Michiel Vandewalle
+ * Github author: https://github.com/michiel-vandewalle
+ * Github project: https://github.com/michiel-vandewalle/Mailchimp-AJAX-form-submit-vanillaJS
+ */
+
+function configfMailchimp() {
+    document.getElementById('signupform').addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        // Check for spam
+        if(document.getElementById('js-validate-robot').value !== '') { return false }
+
+        // Get url for mailchimp
+        var url = this.action.replace('/post?', '/post-json?');
+
+        // Add form data to object
+        var data = '';
+        var inputs = this.querySelectorAll('#js-form-inputs input');
+        for (var i = 0; i < inputs.length; i++) {
+            data += '&' + inputs[i].name + '=' + encodeURIComponent(inputs[i].value);
+        }
+
+        // Create & add post script to the DOM
+        var script = document.createElement('script');
+        script.src = url + data;
+        document.body.appendChild(script);
+
+        // Callback function
+        var callback = 'callback';
+        window[callback] = function(data) {
+
+            // Remove post script from the DOM
+            delete window[callback];
+            document.body.removeChild(script);
+
+            // Display response message
+            document.getElementById('js-subscribe-response').innerHTML = data.msg
+        }
+    })
+}
+
 window.addEventListener('load', function() {
     const signupButton = document.getElementById('sign-up-button-scroller')
     signupButton.addEventListener('click', function(e) {
         let hashval = signupButton.getAttribute('href')
-        console.log(hashval)
         let target = document.querySelector(hashval)
         target.scrollIntoView({
             behavior: 'smooth',
@@ -11,14 +54,5 @@ window.addEventListener('load', function() {
         history.pushState(null, null, hashval)
         e.preventDefault()
     })
-
+    configfMailchimp()
 })
-
-
-window.addEventListener('load', function() {
-    const sendFormButton = document.getElementById('mc-embedded-subscribe')
-    console.log(sendFormButton)
-})
-
-
-//<form action="https://cafe-robot.us18.list-manage.com/subscribe/post?u=343737dedbadf272d034f2cc4&amp;id=52ec62aeee" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="" novalidate>
